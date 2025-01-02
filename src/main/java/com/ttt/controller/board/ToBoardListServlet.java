4ac351117df19e1fa1ac024c0e902bc9b8cfc71c
이민영 <ffrf1234tina@gmail.com>
2025-01-02 15:13:16 +0900

    my/수정/카테고리별 게시판에 카테고리 이름 넣어줌

--- src/main/java/com/ttt/controller/board/ToBoardListServlet.java
+++ src/main/java/com/ttt/controller/board/ToBoardListServlet.java
@@ -23,6 +23,18 @@ public class ToBoardListServlet extends HttpServlet {
 		LocalDate today = LocalDate.now();
 		request.setAttribute("today", today);
 		
+		String categoryNo = request.getParameter("categoryNo");
+		String categoryTitle="";
+		switch (categoryNo) {
+		case "1": categoryTitle="미취학아동 게시판";break;
+		case "2": categoryTitle="초등학생 게시판";break;
+		case "3": categoryTitle="중학생 게시판";break;
+		case "4": categoryTitle="고등학생 게시판";break;
+		case "5": categoryTitle="수험생 게시판";break;
+		case "6": categoryTitle="인기글 게시판";break;
+		default: categoryTitle="error";break;
+		}
+		request.setAttribute("categoryTitle", categoryTitle);
 		request.getRequestDispatcher("/WEB-INF/views/board/boardList.jsp").forward(request, response);
 	}
 
