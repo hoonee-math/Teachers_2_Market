39a3f355f826f56640c52aec797926f34b17e6bd
ahnyoonsang <ahnyoonsang47@gmail.com>
2025-01-02 17:56:16 +0900

    ys/코드 정리

--- src/main/java/com/ttt/controller/member/ToMemberEnrollServlet.java
+++ src/main/java/com/ttt/controller/member/ToMemberEnrollServlet.java
@@ -7,33 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ToMemberEnrollServlet
- */
 @WebServlet("/member/enrollmain")
 public class ToMemberEnrollServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ToMemberEnrollServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/views/enroll/enrollmain.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
