c95e6ad3eda33f119078874487a6f1cf1841397b
ahnyoonsang <ahnyoonsang47@gmail.com>
2025-01-03 11:12:47 +0900

    ys/수정삭제추가/엔롤멤버js삭제, 엔롤멤버js수정, Member엔롤엔드서블릿 작성 중

--- src/main/java/com/ttt/controller/member/MemberEnrollEndServlet.java
+++ src/main/java/com/ttt/controller/member/MemberEnrollEndServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ttt.dto.Member2;
+
 @WebServlet("/member/enrollend")
 public class MemberEnrollEndServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,7 +18,35 @@ public class MemberEnrollEndServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String memberName = request.getParameter("memberName");
+		String memberId = request.getParameter("memberId");
+		
+		String emailId = request.getParameter("emailId");
+		String emailDomain = request.getParameter("emailDomain");
+		String email = emailId + "@" + emailDomain;
+		
+		String memberPw = request.getParameter("memberPw");
+		String memberNick = request.getParameter("memberNick");
+		
+		String addressNo = request.getParameter("addressNo");
+		String addressRoad = request.getParameter("addressRoad");
+		String addressDetail = request.getParameter("addressDetail");
+		String address = "("+addressNo+") "+addressRoad+" "+addressDetail;
+		
+		int eduType = Integer.parseInt(request.getParameter("eduType"));
+		
+		Member2 m = Member2.builder()
+				.memberName(memberName)
+				.memberId(memberId)
+				.memberPw(memberPw)
+				.email(email)
+				.address(address)
+				.eduType(eduType)
+				.build();
+		
+		
+		
+		
 		
 		
 		
