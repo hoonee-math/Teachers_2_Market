8a19415c0fb3e9964c9f9a11c60aaee58f8ec669
이민영 <ffrf1234tina@gmail.com>
2025-01-06 18:47:41 +0900

    my/수정/회원가입시, 구군 학교 선택 로직 구현 중

---
+++ src/main/java/com/ttt/controller/post/SchoolServlet.java
@@ -0,0 +1,56 @@
+package com.ttt.controller.post;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.google.gson.Gson;
+import com.ttt.common.SqlSessionTemplate;
+
+/**
+ * Servlet implementation class SchoolServlet
+ */
+@WebServlet("/post/school")
+public class SchoolServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public SchoolServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String district = request.getParameter("district");
+		String schoolName = request.getParameter("schoolName");
+		
+		Map<String, String> params = new HashMap<>();
+		params.put("district", district);
+		params.put("schoolName", schoolName);
+		
+		List<String> schools = new ArrayList<>();
+		
+		try {
+			SqlSession session = SqlSessionTemplate.getSession();
+			schools = session.selectList("post2.selectSchool",params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		response.setContentType("application/json;charset=UTF-8");
+		new Gson().toJson(schools, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
