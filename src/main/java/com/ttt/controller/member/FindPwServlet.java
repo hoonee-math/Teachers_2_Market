b46bdc713eed60402fd5c50dfbde6c0c4cd334d0
이민영 <ffrf1234tina@gmail.com>
2025-01-06 14:04:36 +0900

    my/생성/비밀번호 찾기 페이지 생성

---
+++ src/main/java/com/ttt/controller/member/FindPwServlet.java
@@ -0,0 +1,26 @@
+package com.ttt.controller.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/findpw")
+public class FindPwServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FindPwServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/enroll/findPw.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
